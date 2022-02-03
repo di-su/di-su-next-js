751e0f7579b48b36714d7dc3faf60a9d6db4d2cf
disu <37470735+di-su@users.noreply.github.com>
2022-02-03 17:31:17 +0000

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
   //   "./pages/**/*.{js,ts,jsx,tsx}",
   //   "./components/**/*.{js,ts,jsx,tsx}",
   // ],
+  mode: "jit",
   purge: ["./build/**/*.html", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     // fontFamily: {
